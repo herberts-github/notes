3cdec37bea9fe97496c573fed0574a4ca11a5d96
herberts <bertsyxx@163.com>
2021-07-20 01:44:23 +0800

    Pygame - 武装飞船 - 重构 - check_events

--- snippet/spacecraft-war/alien_invasion.py
+++ snippet/spacecraft-war/alien_invasion.py
@@ -23,10 +23,7 @@ class AlienInvasion:
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
 
             # 每次循环时都重绘屏幕
             self.screen.fill(self.settings.bg_color)
@@ -34,6 +31,13 @@ class AlienInvasion:
 
             # 让最近绘制的屏幕可见
             pygame.display.flip()
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == '__main__':
