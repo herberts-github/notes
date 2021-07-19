6ab5f2b36120b416d1871dc9f0fdf24d50561230
herberts <bertsyxx@163.com>
2021-07-20 02:17:18 +0800

    Pygame - 武装飞船 - 全屏模式

--- snippet/spacecraft-war/alien_invasion.py
+++ snippet/spacecraft-war/alien_invasion.py
@@ -16,6 +16,11 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+        # 全屏模式
+        # self.screen = pygame.display.set_mode(
+        #     (0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption('外星人入侵')
 
         self.ship = Ship(self)
