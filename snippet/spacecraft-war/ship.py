8b40d035909d11ea7a3062043b0cfe99d66a65b0
herberts <bertsyxx@163.com>
2021-07-20 02:01:50 +0800

    Pygame - 武装飞船 - 调整飞船速度

--- snippet/spacecraft-war/ship.py
+++ snippet/spacecraft-war/ship.py
@@ -7,6 +7,7 @@ class Ship:
     def __init__(self, ai_game):
         """初始化飞船并设置初始位置"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # 加载飞船图像并获取外接矩形
@@ -16,16 +17,23 @@ class Ship:
         # 对于每艘飞船，将分在底部中央位置
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # 在飞船属性 x 中存储小数值
+        self.x = float(self.rect.x)
+
         # 移动标志
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
         """根据移动标志调整飞船位置"""
+        # 更新飞船而不是 rect 对象的x值
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+
+        # 根据 self.x 更新 rect 对象
+        self.rect.x = self.x
 
     def blitme(self):
         """在指定位置绘制飞船"""
