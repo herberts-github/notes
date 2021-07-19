fb9d05f5e24f373fd5b34abeb462df887974bc73
herberts <bertsyxx@163.com>
2021-07-20 01:46:06 +0800

    Pygame - 武装飞船 - 重构 - update_screen

--- snippet/spacecraft-war/alien_invasion.py
+++ snippet/spacecraft-war/alien_invasion.py
@@ -24,13 +24,7 @@ class AlienInvasion:
         """开始游戏的主循环"""
         while True:
             self._check_events()
-
-            # 每次循环时都重绘屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._update_screen()
 
     def _check_events(self):
         """响应按键和鼠标事件"""
@@ -38,6 +32,15 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换新屏幕"""
+        # 每次循环时都重绘屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
