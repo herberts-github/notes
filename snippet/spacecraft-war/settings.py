22c5e51b12775b20663ee24712c3d2609776efdd
herberts <bertsyxx@163.com>
2021-07-21 00:04:56 +0800

    Pygame - 外星人 - 提高子弹的速度

--- snippet/spacecraft-war/settings.py
+++ snippet/spacecraft-war/settings.py
@@ -12,7 +12,7 @@ class Settings:
         self.ship_speed = 1.5
 
         # 子弹设置
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5  # 设置最佳值取决于使用的系统速度
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
