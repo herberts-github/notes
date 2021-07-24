07b6dc16de13f7879dea8613b187d1c59952b25e
herberts <bertsyxx@163.com>
2021-07-24 23:25:33 +0800

    网络 - Socket - 异步多线程Web Server

---
+++ network/thread_socket_server.py
@@ -0,0 +1,68 @@
+# -*- coding:utf-8 -*-
+# 多线程 Web Server
+# 当通过 accept 接受连接之后，就开启一个新的线程来处理这个连接
+# 而主程序可继续在 while True 循环中处理其他连接
+import errno
+import socket
+import threading
+import time
+
+EOL1 = b'\n\n'
+EOL2 = b'\n\r\n'
+body = '''Hello, world! <h1>PYTHON</h1> - from {thread_name}'''
+response_params = [
+    'HTTP/1.0 200 OK',
+    'Date: Sun, 27 may 2018 01:01:01 GMT',
+    'Content-Type: text/plain; charset=utf-8',
+    'Content-Length: {length}\r\n',
+    body,
+]
+response = '\r\n'.join(response_params)
+
+
+def handle_connection(conn, addr):
+    print(conn, addr)
+    # time.sleep(60)
+    request = b""
+    while EOL1 not in request and EOL2 not in request:
+        request += conn.recv(1024)  # 注意在设置为非阻塞模式时这里会有报错，建议自己探索一下问题来源。
+
+    print(request)
+    current_thread = threading.currentThread()
+    content_length = len(body.format(thread_name=current_thread.name).encode())
+    print(current_thread.name)
+    conn.send(response.format(thread_name=current_thread.name, length=content_length).encode())
+    conn.close()
+
+
+def main():
+    # socket.AF_INET    用于服务器与服务器之间的网络通信
+    # socket.SOCK_STREAM    基于TCP的流式socket通信
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # 设置端口可复用，保证我们每次Ctrl C之后，快速再次重启
+    serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serversocket.bind(('127.0.0.1', 8000))
+    # 可参考：https://stackoverflow.com/questions/2444459/python-sock-listen
+    serversocket.listen(10)
+    print('http://127.0.0.1:8000')
+    serversocket.setblocking(1)  # 设置socket为非阻塞模式，当前 socket 不会在 accept 或 recv 时处于阻塞状态
+
+    try:
+        i = 0
+        while True:
+            try:
+                conn, address = serversocket.accept()  # 在非阻塞模式下，当没有连接可被接受时，会抛出 EAGAIN 错误
+            except socket.error as e:
+                if e.args[0] != errno.EAGAIN:
+                    raise
+                continue
+            i += 1
+            print(i)
+            t = threading.Thread(target=handle_connection, args=(conn, address), name='thread-%s' % i)
+            t.start()
+    finally:
+        serversocket.close()
+
+
+if __name__ == '__main__':
+    main()
